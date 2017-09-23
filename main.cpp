fd82a273938cfcce3a8e313bc525a577da8668f1
jclovezq <31952988+jclovezq@users.noreply.github.com>
2017-09-23 10:27:50 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  love
+//
+//  Created by 胡西东 on 17/9/23.
+//  Copyright © 2017年 胡西东. All rights reserved.
+//
+
+#include <iostream>
+
+int  main()
+{
+    cout<<"Where  "
+    <<"are  "
+    <<"you  "
+    <<come  "
+    <<form?"
+    <<endl;
+}
